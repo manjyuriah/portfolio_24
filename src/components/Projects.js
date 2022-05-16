3d41e4a96423413bfbda7429e7e5382ea52dff14
minkim <manjyuriah01@gmail.com>
2022-05-16 15:35:45 +0900

    ADD project 설명 추가

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,90 +1,77 @@
 import './Projects.scss';
+import { useState } from 'react';
 
 function Projects() {
+  const [explain,setExplain]=useState([
+    'PC 웹 버전과 미디어 쿼리를 사용한 Mobile버전으로 만든 룰렛 게임.\n랜덤 함수를 이용해 룰렛 해당 수치에 맞춰 회전각도를 적용한 이벤트 페이지',
+    '고객전용 이벤트 페이지로, 상품 홍보 뿐만아니라 트래픽 증가 효과를 볼 수 있도록 만든 게임 페이지',
+    '기존 OCN 사이트를 리뉴얼 한 팀프로젝트',
+    '다양한 배너 효과를 적용한 사이트로 원스톱 쇼핑 형태로 제작한 팀프로젝트.\n PC용 웹 사이트를 기반으로 개인작업한 모바일 전용 페이지',
+    '동적 웹 쇼핑몰을 제작하기 위한 관리자 페이지로 배너,FAQ,NOTICE,회원관리,상품등록 등 다양한 기능을 사용할 수 있도록 제작.\n 모든 페이지는 백엔드와 함께 제작하였으며 My-Sql을 이용해 데이터를 저장',
+    '관리자페이지를 통한 모든 데이터를 파서하여 제작한 메인페이지.\n 로그인,회원가입 뿐만아니라 결제시스템까지 적용한 쇼핑몰로 데이터베이스를 통해 가입된 고객정보를 확인후 실제 로그인이 가능하도록 제작',
+    '외부에서 제작된 디자인 및 퍼블리싱을 기획서에 맞춰 전면 수정 및 재 구성하는 형태로 제작',
+    '메인페이지1개, 서브페이지3개로 이루어진 팀 프로젝트. Q&A는 입력 후 백엔드로 전달되어 자동으로 JSON이 파싱되도록 제작',
+  ])
+  const [number,setNumber]=useState(0)
   return (
       <>
       <div className="projects-container">
         <div className="projects-box">
           <div className="title">Projects</div>
+          {/* 포폴페이지 프리뷰 */}
+          <div className="preview">
+                <img src={require("../assets/pc.png")} alt="pc"/>
+                <img src={require("../assets/mobile.png")} alt="mobile"/>
+                <div className="explain">{explain[number]}</div>
+          </div>
+          {/* 포폴페이지 목록 및 설명 */}
           <ul className="list">
             <li>
-              <div className='left'>
+              <div className='left' onClick={()=>setNumber(0)}>
                 <div>01</div>
                 <div className="line">룰렛게임 (PC/Mobile)</div>
               </div>
-              <div className="preview">
-                <img src={require("../assets/pc.png")} alt="pc"/>
-                <img src={require("../assets/mobile.png")} alt="mobile"/>
-                <div className="explain">PC 웹 버전과 미디어 쿼리를 사용한 Mobile버전으로 만든 룰렛 게임
-                <br />랜덤 함수를 이용해 룰렛 해당 수치에 맞춰 회전각도를 적용한 이벤트 페이지
-                </div>
-              </div>
             </li>
             <li>
-            <div className='left'>
+            <div className='left' onClick={()=>setNumber(1)}>
               <div>02</div>
               <div className="line">숨은 그림 찾기 (PC)</div>
-              <div className="explain">
-                고객전용 이벤트 페이지로, 상품 홍보 뿐만아니라 트래픽 증가 효과를 볼 수 있도록 만든 게임 페이지
-              </div>
               </div>
             </li>
             <li>
-            <div className='left'>
+            <div className='left' onClick={()=>setNumber(2)}>
               <div>03</div>
               <div className="line">OCN 페이지 (PC)</div>
-              <div className="explain">
-                기존 OCN 사이트를 리뉴얼 한 팀프로젝트
-              </div>
               </div>
             </li>
             <li>
-            <div className='left'>
+            <div className='left' onClick={()=>setNumber(3)}>
               <div>04</div>
               <div className="line">Hello Pet (PC/Mobile)</div>
-              <div className="explain">
-                다양한 배너 효과를 적용한 사이트로 원스톱 쇼핑 형태로 제작한 팀프로젝트<br />
-                PC용 웹 사이트를 기반으로 개인작업한 모바일 전용 페이지
-              </div>
               </div>
             </li>
             <li>
-            <div className='left'>
+            <div className='left' onClick={()=>setNumber(4)}>
               <div>05</div>
               <div className="line">한우사랑 관리자 페이지 (PC)</div>
-              <div className="explain">
-                동적 웹 쇼핑몰을 제작하기 위한 관리자 페이지로 배너,FAQ,NOTICE,회원관리,상품등록 등 다양한 기능을 사용할 수 있도록 제작<br />
-                모든 페이지는 백엔드와 함께 제작하였으며 My-Sql을 이용해 데이터를 저장
-              </div>
               </div>
             </li>
             <li>
-            <div className='left'>
+            <div className='left' onClick={()=>setNumber(5)}>
               <div>06</div>
               <div className="line">한우사랑 메인 페이지 (PC)</div>
-              <div className="explain">
-                관리자페이지를 통한 모든 데이터를 파서하여 제작한 메인페이지<br />
-                로그인,회원가입 뿐만아니라 결제시스템까지 적용한 쇼핑몰로 데이터베이스를 통해 가입된 고객정보를 확인후 실제 로그인이 가능하도록 제작
-              </div>
               </div>
             </li>
             <li>
-            <div className='left'>
+            <div className='left' onClick={()=>setNumber(6)}>
               <div>07</div>
               <div className="line">의류 쇼핑몰 (Mobile)</div>
-              <div className="explain">
-                외부에서 제작된 디자인 및 퍼블리싱을 기획서에 맞춰 전면 수정 및 재 구성하는 형태로 제작
-              </div>
               </div>
             </li>
             <li>
-            <div className='left'>
+            <div className='left' onClick={()=>setNumber(7)}>
               <div>08</div>
               <div className="line">조명 사이트 (PC)</div>
-              <div className="explain">
-                메인페이지1개, 서브페이지3개로 이루어진 팀 프로젝트<br />
-                Q&A는 입력 후 백엔드로 전달되어 자동으로 JSON이 파싱되도록 제작
-              </div>
               </div>
             </li>
           </ul>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,18 +7,78 @@ function Skills() {
         <div className="skills-box">
           <div className="title">Skills</div>
           <ul className="list">
-            <li><div>HTML & CSS</div></li>
-            <li><div>Scss</div></li>
-            <li><div>Javascript</div></li>
-            <li><div>Jquery</div></li>
-            <li><div>ECMA</div></li>
-            <li><div>Ajax</div></li>
-            <li><div>Vue</div></li>
-            <li><div>Git & Github</div></li>
-            <li><div>XML & JSON</div></li>
-            <li><div>API</div></li>
-            <li><div>Responsive Web</div></li>
-            <li><div>Etc</div></li>
+            <li>
+              <div>HTML & CSS</div>
+              <div className='explain'>
+                DOCTYPE을 이해하고 디자인에 맞춰 작업이 가능. name,id,class분류를 알고 있고 Method에 대한 POST, GET에 대한 원리도 이해. Import, Keyframe, Flex, Grid 등 각종 스타일 선언 방식 이해
+              </div>
+            </li>
+            <li>
+              <div>Scss</div>
+              <div className='explain'>
+                기본적으로 전처리기로 작성이 가능하며, CSS 컴파일 가능. 그 외에 코드 중복을 최소화 하며 문법에 대한 작성 및 유지보수 경험도 풍부 변수, 함수 모두 구현 가능
+              </div>
+            </li>
+            <li>
+              <div>Javascript</div>
+              <div className='explain'>
+                함수, 메소드, 클래스, 프로토타입, 이벤트, 루프 클로져, 디바운싱 등 다양한 응용이 가능하며 파라미터 데이터 활용과 함께 Back-end와의 작업 경험
+              </div>
+            </li>
+            <li>
+              <div>Jquery</div>
+              <div className='explain'>
+                Javascript를 기본으로 하여 DOM, Node에 대한 다양한 활용 프로그램이 가능하며, 웹 속도의 효율을 고려한 Jquery엔진을 커스텀 할 수 있습니다. 배열에 활용 능력을 가지고 있으며, 풀로그인 API를 활용
+              </div>
+              </li>
+            <li>
+              <div>ECMA</div>
+              <div className='explain'>
+                import와 export를 기본으로 코드를 작성하며, BOM, DOM의 확장성을 활용할 수 있습니다. Class와 Module모두 사용 가능하며, fetch를 이용한 배열 데이터를 활용
+              </div>
+            </li>
+            <li>
+              <div>Ajax</div>
+              <div className='explain'>
+                동기,비동기 통신이 가능하며 동적 화면 출력 및 DOM, NODE 모두 사용 가능.기본적인 통신개념과 XML, JSON 파일에 대한 파서 개념을 알고 send 함수를 통해 데이터 전송도 가능
+              </div>
+            </li>
+            <li>
+              <div>Vue</div>
+              <div className='explain'>
+                기본 CDN 형태의 Vue를 사용하며, Template과 Component를 사용할 수 있습니다. 인스턴스 생성, 화면 부착, 갱신, 소멸을 단계별로 구분하여 사용할 수 있습니다. 배열 데이터를 기본으로 화면 렌더링을 하고, 각종 이벤트 함수를 응용
+              </div>
+            </li>
+            <li>
+              <div>Git & Github</div>
+              <div className='explain'>
+                분산형 관리 시스템을 경험 하였으며, 프로젝트 구성 및 Push, Pull request 같은 이벤트에 반응하여 자동으로 작업(배포 등)을 경험 하였습니다. Git 기본 사용공간은 Public을 기본
+              </div>
+            </li>
+            <li>
+              <div>XML & JSON</div>
+              <div className='explain'>
+                태그와 다차원 배열에 대한 경험이 있으며, MySQL Database 값에 대한 파싱을 통한 파서 데이터 출력도 가능
+              </div>
+            </li>
+            <li>
+              <div>API</div>
+              <div className='explain'>
+                도로명 주소, 지도, 결제 시스템 등 다양한 API 경험이 있으며, 웹사이트의 조건 맞게 커스텀도 가능
+              </div>
+            </li>
+            <li>
+              <div>Responsive Web</div>
+              <div className='explain'>
+                화면 너비에 따라 유동적으로 레이아웃을 변화 시킬 수 있으며, Cross Browsing 기술 플랫폼으로 제작
+              </div>
+            </li>
+            <li>
+              <div>Etc</div>
+              <div className='explain'>
+                VS Code를 사용하였으며, 관리자 페이지에 대한 적용 원리 지식과 Webmaster도구틀을 이용한 SEO작업 가능
+              </div>
+            </li>
           </ul>
         </div>
       </div>
