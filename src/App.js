2673f095bdb08b6d54adefb273a60f049efd68b6
minkim <manjyuriah01@gmail.com>
2022-05-17 10:11:40 +0900

    ADD 스크롤 이벤트 추가

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import Skills from './components/Skills';
 import Footer from './components/Footer';
 import Main from './components/Main';
 import Projects from './components/Projects';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
-
-  
+//페이지 스크롤 이벤트
+//0 , 921 , 1824
 function App() {
   const [ScrollY, setScrollY] = useState(0);  // 스크롤값을 저장하기 위한 상태
   const handleFollow = () => {
     setScrollY(window.pageYOffset); // window 스크롤 값을 ScrollY에 저장
   }
+
+  useEffect(() => {
+    // console.log(ScrollY); // ScrollY가 변화할때마다 값을 콘솔에 출력
+  }, [ScrollY])
+
   useEffect(() => {
     const watch = () => {
       window.addEventListener('scroll', handleFollow);
@@ -22,9 +26,10 @@ function App() {
       window.removeEventListener('scroll', handleFollow); // addEventListener 함수를 삭제
     }
   })
+
   return (
     <div className="App">
-      <Main />
+      <Main ScrollY={ScrollY}/>
       <Skills />
       <Projects />
       <Footer />
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,43 @@
 import './Main.scss';
 import KakaoQR from './KakaoQR';
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
-function Main() {
+function Main(props) {
+
   const [openkakao, setOenkakao] = useState(false);
   function kakoOpen(){
       setOenkakao(!openkakao);
   };
+
+//페이지 스크롤 이벤트
+//0 , 921 , 1824
+  const handleAbout = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  }
+  const handleSkills = () => {
+    window.scrollTo({
+      top: 921,
+      behavior: "smooth"
+    });
+  }
+
+  const handleProjects = () => {
+    window.scrollTo({
+      top: 1824,
+      behavior: "smooth"
+    });
+  }
+
   return (
       <>
       <div className="main-container">
         <ul className="nav">
-          <li>About</li>
-          <li>Skills</li>
-          <li>Projects</li>
+          <li onClick={handleAbout}>About</li>
+          <li onClick={handleSkills}>Skills</li>
+          <li onClick={handleProjects}>Projects</li>
         </ul>
         <div className="main-box">
             <div className="hello">
