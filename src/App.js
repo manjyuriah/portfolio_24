93589cf2560d99049658dda8f1c57dd7a8a31569
김민주 <minjoo@gimminjus-MacBook-Pro.local>
2024-06-20 12:37:08 +0900

    fix : 불필요한 코드 삭제

--- src/App.js
+++ src/App.js
@@ -7,23 +7,20 @@ import React, { useEffect, useState } from 'react';
 
 //페이지 스크롤 이벤트
 //0 , 921 , 1824
-function App() {
-  const [ScrollY, setScrollY] = useState(0);  // 스크롤값을 저장하기 위한 상태
+const App = () => {
+  const [ScrollY, setScrollY] = useState(0);
   const handleFollow = () => {
-    setScrollY(window.pageYOffset); // window 스크롤 값을 ScrollY에 저장
+    setScrollY(window.pageYOffset);
   }
 
-  useEffect(() => {
-    // console.log(ScrollY); // ScrollY가 변화할때마다 값을 콘솔에 출력
-  }, [ScrollY])
 
   useEffect(() => {
     const watch = () => {
       window.addEventListener('scroll', handleFollow);
     }
-    watch(); // addEventListener 함수를 실행
+    watch();
     return () => {
-      window.removeEventListener('scroll', handleFollow); // addEventListener 함수를 삭제
+      window.removeEventListener('scroll', handleFollow);
     }
 
   })
